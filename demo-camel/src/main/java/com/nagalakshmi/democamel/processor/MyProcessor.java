74e7aa0abd4547093f108fbf208087aa09c63fa9
nagalakshmiposam <posamnagalakshmi@gmail.com>
2021-02-16 18:36:55 +0530

    commit init:

---
+++ demo-camel/src/main/java/com/nagalakshmi/democamel/processor/MyProcessor.java
@@ -0,0 +1,28 @@
+package com.nagalakshmi.democamel.processor;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class MyProcessor implements Processor {
+    private static Logger logger = LogManager.getLogger(MyProcessor.class);
+
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        String messageBody = exchange.getIn().getBody(String.class);
+        Map<String, Object> exchangeproperties = exchange.getProperties();
+        logger.info("message body from incoming exchange,{}", messageBody);
+        logger.info("exchange properties, {}", exchangeproperties);
+        messageBody = messageBody.concat("updating the body using processor");
+        logger.info("updated the meassage body exchange, {}", messageBody);
+        exchange.getIn().setBody(messageBody);
+
+
+    }
+}
--- demo-camel/src/main/java/com/nagalakshmi/democamel/routes/NewFile.java
+++ demo-camel/src/main/java/com/nagalakshmi/democamel/routes/NewFile.java
@@ -1,13 +1,39 @@
 package com.nagalakshmi.democamel.routes;
 
+import ch.qos.logback.classic.Logger;
+import com.nagalakshmi.democamel.processor.MyProcessor;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.ValidatorBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import static java.lang.ProcessBuilder.Redirect.to;
 
 @Component
 public class NewFile extends RouteBuilder {
+
+    @Value("${app.source}")
+    private String sourcelocation;
+    @Value("${app.destination}")
+    private String destination;
+    @Autowired
+    private MyProcessor Processor;
+
     @Override
     public void configure() throws Exception {
-        from("file:C:/training/input/").to("file:C:/training/output/");
+        from("file:" + sourcelocation)
+                .routeId("demo-route-ID")
+//                .setBody(simple ( "${body}. file has been updated"))
+
+                .process(Processor)
+                .to("file:" + destination)
+
+
+                .log(LoggingLevel.INFO, "file is moved to destination \n ${body}");
 
     }
+
+
 }
